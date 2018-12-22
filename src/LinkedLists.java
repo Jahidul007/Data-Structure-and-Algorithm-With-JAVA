f76ebe19b312887067c2a92d07124ec1cedd3883
Jahidul Islam <jahidul479@gmail.com>
2018-12-22 21:52:34 +0600

    commit

---
+++ src/LinkedLists.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedLists {
+    public static void main(String[] args) {
+
+        String[] things = {"apples","bacon","pie","eggs","goats"};
+
+        List<String> list1 = new LinkedList<String>();
+    }
+}
