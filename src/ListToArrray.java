29a2aa554cd6569bfa39442ea37f16c1feb9b2fa
Jahidul Islam <jahidul479@gmail.com>
2018-12-24 21:04:43 +0600

    commit

---
+++ src/ListToArrray.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class ListToArrray {
+    public static void main(String[] args) {
+
+        String[] stuff = {"babbies", "watermelons", "fudge", "melons"};
+
+        LinkedList<String> theList = new LinkedList<String>(Arrays.asList(stuff));
+
+        
+    }
+}
