cdb033b44d96477de0557649fcf073caa1f61f12
Jahidul Islam <jahidul479@gmail.com>
2019-01-01 21:55:06 +0600

    commit

--- src/QueensProblem.java
+++ src/QueensProblem.java
@@ -39,7 +39,22 @@ public class QueensProblem {
 
     private boolean isPlaceValid(int rowIndex, int colIndex) {
 
-        return false;
+        for (int i = 0; i < colIndex; i++) {
+            if (chessTable[rowIndex][i] == 1) {
+                return false;
+            }
+        }
+        for (int i = rowIndex, j = colIndex; i >= 0 && j >= 0; i--, j--) {
+            if (chessTable[i][j] == 1) {
+                return false;
+            }
+        }
+        for (int i = rowIndex, j = colIndex; i < chessTable.length && j >= 0; i++, j--) {
+            if (chessTable[i][j] == 1) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private void printQueens() {
@@ -54,5 +69,10 @@ public class QueensProblem {
             System.out.println();
         }
         System.out.println();
+    }
+
+    public static void main(String[] args) {
+        QueensProblem problem = new QueensProblem(32);
+        problem.solve();
     }
 }
