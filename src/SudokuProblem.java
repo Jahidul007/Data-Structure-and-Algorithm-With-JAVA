2baaa54b4c4dd688b1614fc78fe5d304a15fcd22
Jahidul Islam <jahidul479@gmail.com>
2019-01-04 14:52:56 +0600

    backTracking

--- src/SudokuProblem.java
+++ src/SudokuProblem.java
@@ -83,7 +83,7 @@ public class SudokuProblem {
     }
 
     public static void main(String[] args) {
-        int[][] sudokuTable = {
+        int[][] sudokuTab = {
                 {3, 0, 6, 5, 0, 8, 4, 0, 0},
                 {5, 2, 0, 0, 0, 0, 0, 0, 0},
                 {0, 8, 7, 0, 0, 0, 0, 3, 1},
@@ -105,7 +105,7 @@ public class SudokuProblem {
                 {0,2,0,0,0,0,6,0,0},
                 {0,0,7,5,0,9,0,0,0}
         };
-        SudokuProblem problem = new SudokuProblem(sudokuTable);
+        SudokuProblem problem = new SudokuProblem(sudokuTab);
         problem.solveProblem();
     }
 }
